7bf45ccf83ad01352a7e58b9638a18cdc46f910d
Cecchy2 <dariocecchinato@gmail.com>
2024-07-03 16:22:03 +0200

    creata fetch per i commenti

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -7,14 +7,36 @@ const MovieDetails = () => {
   const { movieId } = useParams();
   const [movieDetails, setMovieDetails] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [imdb, setImdb] = useState("");
+  const [reviews, setReviews] = useState([]);
 
   const fetchMovieDetails = async () => {
     try {
       const response = await fetch(`http://www.omdbapi.com/?apikey=64766599&i=${movieId}`);
       if (response.ok) {
         const details = await response.json();
+        console.log(details);
         setMovieDetails(details);
         setLoading(false);
+        setImdb(details.imdbID);
+        fetchComments(details.imdbID);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const fetchComments = async (imdbId) => {
+    try {
+      const response = await fetch(`https://striveschool-api.herokuapp.com/api/comments/${imdbId}`, {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZiZjlhOTdjMjM5YzAwMTUyZjRiM2QiLCJpYXQiOjE3MjAwMTUzNjYsImV4cCI6MTcyMTIyNDk2Nn0.rgj31Ok017jPG_rhlJkAupaBCr849UWEW0ydNSADBI4",
+        },
+      });
+      if (response.ok) {
+        const comments = await response.json();
+        console.log(comments);
       }
     } catch (err) {
       console.log(err);
